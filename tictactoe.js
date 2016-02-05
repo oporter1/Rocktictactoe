7de25fadccb999d4b07e4e2728bf96df40469bfa
olivia and mike <olivia.mike@learn.com>
2016-02-05 15:43:21 -0800

    updated with tom brady

--- tictactoe.js
+++ tictactoe.js
@@ -1,28 +1,33 @@
 var x=true;
+var board = [0,0,0,
+             0,0,0,
+             0,0,0];
+var numberOfTurnsLeft = 9;
+var stop;
 
-
-function doItWithX(element) {
-  element.append("<img src ='X.png'/>");
-  element.unbind('click');
+function doItWithX(td, rowOffset) {
+  td.append("<img src ='X.png'/>");
+  td.unbind('click');
+  board[td.index()+rowOffset] = 1;
 };
 // a function to change a blank table data box to O img
-function doItWithO(element) {
-  element.append("<img src = 'O.png'/>");
-  element.unbind('click');
+function doItWithO(td, rowOffset) {
+  td.append("<img src = 'O.png'/>");
+  td.unbind('click');
+  board[td.index() + rowOffset] = -1;
 };
 
-function placeMarker(element){
+function placeMarker(tdId, rowOffset){
   if (x===true){
-    doItWithX(element);
+    doItWithX($("#"+tdId), rowOffset);
     x = false;
   }
   else {
-    doItWithO(element);
-    x = true;
+    doItWithO($("#"+tdId), rowOffset);
+    x=true;
   }
 };
 
-var numberOfTurnsLeft = 9;
 function gameCounter () {
   numberOfTurnsLeft= numberOfTurnsLeft-1;
   console.log(numberOfTurnsLeft);
@@ -32,64 +37,163 @@ function gameCounter () {
   }
 };
 
+function stopClicking () {
+  if (stop===true) {
+    // alert("stop");
+  $(".tableData").unbind('click'); }
+};
 
 //  jquery
 $(document).ready(function(){
+  //functions to click on specific boxes and game counter function
+  $("#0").click(function(){
+    rowOffset = 0;
+    placeMarker("0", rowOffset);
+    whoWon(board);
+    gameCounter();
+    stopClicking();
+  });
 
-  $("#tableData1").click(function(){
-    placeMarker($("#tableData1"));
+  $("#1").click(function(){
+    rowOffset = 0;
+    placeMarker("1", rowOffset);
+    whoWon(board);
     gameCounter();
+    stopClicking();
   });
-  $("#tableData2").click(function(){
-    placeMarker($("#tableData2"));
-    gameCounter();
-  });
-  $("#tableData3").click(function(){
-    placeMarker($("#tableData3"));
-    gameCounter();
-  });
-  $("#tableData4").click(function(){
-    placeMarker($("#tableData4"));
-    gameCounter();
-  });
-  $("#tableData5").click(function(){
-    placeMarker($("#tableData5"));
-    gameCounter();
-  });
-  $("#tableData6").click(function(){
-    placeMarker($("#tableData6"));
-    gameCounter();
-  });
-  $("#tableData7").click(function(){
-    placeMarker($("#tableData7"));
-    gameCounter();
-  });
-  $("#tableData8").click(function(){
-    placeMarker($("#tableData8"));
-    gameCounter();
-  });
-  $("#tableData9").click(function(){
-    placeMarker($("#tableData9"));
-    gameCounter();
 
+  $("#2").click(function(){
+    rowOffset=0;
+    placeMarker("2", rowOffset);
+    whoWon(board);
+    gameCounter();
+    stopClicking();
   });
+
+  $("#3").click(function(){
+    rowOffset = 3;
+    placeMarker("3", rowOffset);
+    whoWon(board);
+    gameCounter();
+    stopClicking();
+  });
+
+  $("#4").click(function(){
+    rowOffset = 3;
+    placeMarker("4", rowOffset);
+    whoWon(board);
+    gameCounter();
+    stopClicking();
+  });
+
+  $("#5").click(function(){
+    rowOffset = 3;
+    placeMarker("5", rowOffset);
+    whoWon(board);
+    gameCounter();
+    stopClicking();
+  });
+
+  $("#6").click(function(){
+    rowOffset = 6;
+    placeMarker("6", rowOffset);
+    whoWon(board);
+    gameCounter();
+    stopClicking();
+  });
+
+  $("#7").click(function(){
+    rowOffset = 6;
+    placeMarker("7", rowOffset);
+    whoWon(board);
+    gameCounter();
+    stopClicking();
+  });
+
+  $("#8").click(function(){
+    rowOffset=6;
+    placeMarker("8", rowOffset);
+    whoWon(board);
+    gameCounter();
+    stopClicking();
+  });
+
+  // function to add array index postions to determine a winner
+  function sumElements(arr, index0, index1, index2) {
+    return arr[index0] + arr[index1] + arr[index2];
+  };
+
+  function whoWon(arr){
+    if (sumElements(arr, 0, 1, 2) === -3){
+      $("#header").html("O Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 3, 4, 5) === -3){
+      $("#header").html("O Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 6, 7, 8) === -3){
+      $("#header").html("O Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 0, 3, 6) === -3){
+      $("#header").html("O Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 1, 4, 7) === -3){
+      $("#header").html("O Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 2, 5, 8) === -3){
+      $("#header").html("O Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 0, 4, 8) === -3){
+      $("#header").html("O Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 2, 4, 6) === -3){
+      $("#header").html("O Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 0, 1, 2) === 3) {
+      $("#header").html("X Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 3, 4, 5) === 3){
+      $("#header").html("X Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 6, 7, 8) === 3){
+      $("#header").html("X Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 0, 3, 6) === 3){
+      $("#header").html("X Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 1, 4, 7) === 3){
+      $("#header").html("X Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 2, 5, 8) === 3){
+      $("#header").html("X Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 0, 4, 8) === 3){
+      $("#header").html("X Won!");
+      stop= true;
+    }
+    else if (sumElements(arr, 2, 4, 6) === 3){
+      $("#header").html("X Won!");
+      stop= true;
+    }
+
+     else ("");
+
+  };
+
+
 
 //when the board is full disable game and alert that the game is over
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
